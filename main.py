ea408dafa827481e9cf1e10d2679ca64f0c17323
ilybe <zaraharuein@gmail.com>
2023-03-22 16:02:36 -0400

    Hello

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ def encode(userinput):
         element = int(element)
         element += 3
         new += str(int(element))
-    print(new)
+    return new
 
+def decode(encoded_pass): #Zara Haruna
+    old = " "
+    for num in encoded_pass: # Subtracts 3 from each digit in encoded password
+        num = str(int(num) - 3)
+        old += num # Concatenates new digits together in empty string
+    return old
 
 def main():
-    print("Menu\n"
-          "-------------\n"
-          "1. Encode\n"
-          "2. Decode\n"
-          "3. Quit\n")
-    user = input("Please enter an option: ")
+
     while True:
+        print("Menu\n"
+              "-------------\n"
+              "1. Encode\n"
+              "2. Decode\n"
+              "3. Quit\n")
+        user = input("Please enter an option: ")
         if user == "1":
+            global userinput # Allows original password to be applied to the encode and decode functions
             userinput = input("Please enter your password to encode: ")
             print(encode(userinput))
             print("Your password has been encoded and stored!")
         if user == "2":
-            pass
+            print(f"The encoded password is {encode(userinput)}, and the original password is{decode(encode(userinput))}.")
         if user == "3":
             break
 
